0486fbccd7e1b9a0c40cda6014e503fa54afea60
Mohammad Arif <codeinkotlang@gmail.com>
2025-02-25 21:25:45 +0530

    some changes

--- src/main/kotlin/presentation/config/Koin.kt
+++ src/main/kotlin/presentation/config/Koin.kt
@@ -2,12 +2,13 @@ package com.synac.presentation.config
 
 import com.synac.di.koinModule
 import io.ktor.server.application.*
+import org.koin.core.logger.Level
 import org.koin.ktor.plugin.Koin
 import org.koin.logger.slf4jLogger
 
 fun Application.configureKoin() {
     install(Koin) {
-        slf4jLogger()
+        slf4jLogger(level = Level.WARNING)
         modules(koinModule)
     }
 }
